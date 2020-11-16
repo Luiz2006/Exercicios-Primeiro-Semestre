5b8ee17395b39f27f90e30149e2453fcc7d602d3
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2020-11-16 17:53:37 -0300

    :white_check_mark: Updated exercicio 3

--- atividade10/exercicio03.c
+++ atividade10/exercicio03.c
@@ -1,28 +1,31 @@
 /*
 Name: Luiz Araujo
-Description: Elabore duas funções, 
-além da função principal (main) que 
-calcule duas operações aritméticas consecutivas em três valores e duas operações lidas na função principal. 
+Description: Elabore duas funções,
+além da função principal (main) que
+calcule duas operações aritméticas consecutivas em três valores e duas operações lidas na função principal.
 Na outra função verifique se o resultado das operações produzirá um número maior, menor ou igual a zero (0).
 */
 
 #include<stdio.h>
-#include<stdlib.h>
-#include<locale.h>
+//#include<stdlib.h>
+//#include<locale.h>
 
 int main(void){
-	setlocale(LC_ALL, "Portuguese");
-	fflush(stdin);
-	int valor[3];
-	char operador1;
+//	setlocale(LC_ALL, "Portuguese");
 
-	printf("Informe três valores: \n");
-	scanf("%i%i%i", &valor[0], &valor[1], &valor[2]);
-	printf("Informe dois operadores:\n");
+	//int valor[3];
+	char operador1, operador2;
+
+	//printf("Informe três valores: \n");
+//	scanf("%i%i%i", &valor[0], &valor[1], &valor[2]);
+	printf("Informe um operador:\n");
 	scanf("%c", &operador1);
-//	scanf("%c", operador2);
-	
-	
-	
-	
+	fflush(stdin);
+	printf("Informe outro operador:\n");
+	scanf("%c", &operador2);
+    printf("%c e %c", operador1, operador2);
+
+
+
+
 }
